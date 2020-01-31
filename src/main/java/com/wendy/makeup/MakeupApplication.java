f9b225dcbac08da1c820b54b91a701f54f2e168b
farvher <fesanmiguel@misena.edu.co>
2020-01-30 21:53:32 -0500

    mi primer commit

---
+++ src/main/java/com/wendy/makeup/MakeupApplication.java
@@ -0,0 +1,13 @@
+package com.wendy.makeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendy/makeup/MakeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendy.makeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
